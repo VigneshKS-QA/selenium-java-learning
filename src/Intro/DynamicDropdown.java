17e558b74d6f9e8b8a31c6b9a4297cee3492a96b
VigneshKS-QA <vigneshkssomu26@gmail.com>
2025-05-05 15:28:54 +0530

    Added DynamicDropdown.java file

---
+++ src/Intro/DynamicDropdown.java
@@ -0,0 +1,29 @@
+package Intro;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DynamicDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000);
+		//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();		//(//a[@value='MAA'])[2] is index method, have same element in diff dropdown so used index 2
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();	//parent child relationship (without index)
+		
+		
+		driver.close();
+
+	}
+
+}
